fa0bc106c3d533b6df34e94641978ae278b5b7fb
Jean-Louis Zola <jlzola@gmail.com>
2025-04-05 01:23:37 +0200

    Message commandement, Refacto, Aide en ligne

--- app.js
+++ app.js
@@ -51,5 +51,5 @@ app.use('/', indexRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port} >>> http://localhost:${port}`);
 });
